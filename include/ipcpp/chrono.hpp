36dbae8394d94472d4f2fb6499698fbac55ae4ce
Uy Ha <hchanuy@gmail.com>
2022-02-18 00:12:46 +0100

    Simplify `to_timespec` implementation and add test for it

--- include/ipcpp/chrono.hpp
+++ include/ipcpp/chrono.hpp
@@ -11,9 +11,8 @@ constexpr auto to_timespec(std::chrono::time_point<std::chrono::system_clock, Du
   auto timepoint_s  = time_point_cast<seconds>(timepoint);
   auto timepoint_ns = time_point_cast<nanoseconds>(timepoint);
 
-  auto sec = timepoint_s.time_since_epoch().count();
-  auto nsec =
-      timepoint_ns.time_since_epoch().count() - time_point_cast<nanoseconds>(timepoint_s).time_since_epoch().count();
+  auto sec  = timepoint_s.time_since_epoch().count();
+  auto nsec = timepoint_ns.time_since_epoch().count() % 1'000'000'000;
 
   return {.tv_sec = sec, .tv_nsec = nsec};
 }
---
+++ tests/chrono.test.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch.hpp>
+#include <ipcpp/chrono.hpp>
+#include <ostream>
+
+constexpr auto operator==(::timespec lhs, ::timespec rhs) noexcept -> bool {
+  return lhs.tv_sec == rhs.tv_sec and lhs.tv_nsec == rhs.tv_nsec;
+}
+
+auto operator<<(std::ostream &os, ::timespec const &timespec) -> std::ostream & {
+  return os << "{seconds = " << timespec.tv_sec << ", nanoseconds = " << timespec.tv_nsec << "}";
+}
+
+using namespace ipcpp;
+using namespace std::chrono;
+using namespace std::chrono_literals;
+
+TEST_CASE("to_timespec") {
+  CHECK(ipcpp::to_timespec(system_clock::time_point{}) == ::timespec{});
+  CHECK(ipcpp::to_timespec(system_clock::time_point{} + 2s) == ::timespec{.tv_sec = 2, .tv_nsec = 0});
+  CHECK(ipcpp::to_timespec(system_clock::time_point{} + 2s + 2ns) == ::timespec{.tv_sec = 2, .tv_nsec = 2});
+  CHECK(ipcpp::to_timespec(system_clock::time_point{} + 2s + 999'999'999ns) == ::timespec{.tv_sec = 2, .tv_nsec = 999'999'999});
+  CHECK(ipcpp::to_timespec(system_clock::time_point{} + 2s + 1'999'999'999ns) == ::timespec{.tv_sec = 3, .tv_nsec = 999'999'999});
+}
