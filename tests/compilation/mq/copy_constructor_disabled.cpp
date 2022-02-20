50963468bd2e49cd4ee2071b95a6c13dd6fe2c75
Uy Ha <hchanuy@gmail.com>
2022-02-20 20:53:57 +0100

    Add more tests for deleted copy {assignment,constructor}

---
+++ tests/compilation/mq/copy_constructor_disabled.cpp
@@ -0,0 +1,11 @@
+#include <lpipp/mq.hpp>
+using namespace lpipp;
+using namespace mq_constants;
+
+int main() {
+  auto name        = "/mq-copy-assignment-disabled";
+  auto const queue = *mq::open(name, read_only);
+  auto copy_queue  = *mq::open(name, read_only);
+  copy_queue       = queue;
+  (void)mq::unlink(name);
+}
