87ab6f4f6f7029b799fd9a0ddcc7de00f20666bc
Uy Ha <hchanuy@gmail.com>
2022-02-22 21:06:58 +0100

    Move enum and map functions inside the `fcntl` class

--- include/lpipp/fcntl.hpp
+++ include/lpipp/fcntl.hpp
@@ -9,23 +9,6 @@
 #include <unistd.h>
 
 namespace lpipp {
-enum class DupError : int { fd_limit_reached };
-[[nodiscard]] constexpr auto map_dup_error(int error) noexcept -> DupError {
-  switch (error) {
-  case EMFILE: return DupError::fd_limit_reached;
-  default: return static_cast<DupError>(error);
-  }
-}
-
-enum class DupAtLeastError : int { fd_invalid, fd_limit_reached };
-[[nodiscard]] constexpr auto map_dup_at_least_error(int error) noexcept -> DupAtLeastError {
-  switch (error) {
-  case EINVAL: return DupAtLeastError::fd_invalid;
-  case EMFILE: return DupAtLeastError::fd_limit_reached;
-  default: return static_cast<DupAtLeastError>(error);
-  }
-}
-
 template <typename T>
 class fcntl {
 public:
@@ -33,6 +16,7 @@ public:
     return static_cast<T const &>(*this).m_fd;
   }
 
+  enum class DupError : int { fd_limit_reached };
   [[nodiscard]] auto duplicate() const noexcept -> tl::expected<T, DupError> {
     auto new_fd = ::dup(get_handle());
     if (new_fd == -1) {
@@ -41,12 +25,28 @@ public:
     return T{new_fd};
   }
 
+  enum class DupAtLeastError : int { fd_invalid, fd_limit_reached };
   [[nodiscard]] auto duplicate_at_least(int minimum_fd) const noexcept -> tl::expected<T, DupAtLeastError> {
     auto new_fd = ::fcntl(get_handle(), F_DUPFD, minimum_fd);
     if (new_fd == -1) {
       return tl::unexpected{map_dup_at_least_error(errno)};
     }
     return T{new_fd};
+  }
+
+private:
+  [[nodiscard]] static constexpr auto map_dup_error(int error) noexcept -> DupError {
+    switch (error) {
+    case EMFILE: return DupError::fd_limit_reached;
+    default: return static_cast<DupError>(error);
+    }
+  }
+  [[nodiscard]] static constexpr auto map_dup_at_least_error(int error) noexcept -> DupAtLeastError {
+    switch (error) {
+    case EINVAL: return DupAtLeastError::fd_invalid;
+    case EMFILE: return DupAtLeastError::fd_limit_reached;
+    default: return static_cast<DupAtLeastError>(error);
+    }
   }
 };
 } // namespace lpipp
