bcf7d7830d1b2d98333f32565f945c7ecfbd575a
Uy Ha <hchanuy@gmail.com>
2022-02-28 00:07:56 +0100

    Add macros for defining bitand operator

--- include/lpipp/macros.hpp
+++ include/lpipp/macros.hpp
@@ -18,3 +18,22 @@
 
 #define LPIPP_BITOR_OP(...)                                                                                            \
   LPIPP_BITOR_OVERRIDE(__VA_ARGS__, LPIPP_DIFF_TYPE_BITOR_OP, LPIPP_SAME_TYPE_BITOR_OP, dummy)(__VA_ARGS__)
+
+#define LPIPP_BITAND_OVERRIDE(_1, _2, NAME, ...) NAME
+
+#define LPIPP_BITAND_OP_IMPL(left_type, right_type)                                                                    \
+  friend constexpr auto operator&(left_type lhs, right_type rhs) noexcept->bool {                                      \
+    return static_cast<std::underlying_type_t<left_type>>(lhs)                                                         \
+           & static_cast<std::underlying_type_t<right_type>>(rhs);                                                    \
+  }
+
+#define LPIPP_SAME_TYPE_BITAND_OP(input_type) LPIPP_BITAND_OP_IMPL(input_type, input_type)
+#define LPIPP_DIFF_TYPE_BITAND_OP(left_type, right_type)                                                               \
+  LPIPP_BITAND_OP_IMPL(left_type, right_type)                                                                           \
+                                                                                                                       \
+  friend constexpr auto operator&(right_type lhs, left_type rhs) noexcept->bool {                                      \
+    return rhs & lhs;                                                                                                  \
+  }
+
+#define LPIPP_BITAND_OP(...)                                                                                           \
+  LPIPP_BITAND_OVERRIDE(__VA_ARGS__, LPIPP_DIFF_TYPE_BITAND_OP, LPIPP_SAME_TYPE_BITAND_OP, dummy)(__VA_ARGS__)
