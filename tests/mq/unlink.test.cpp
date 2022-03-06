50b030a13ac78678e57e8b161debdf2941b8f8e9
Uy Ha <hchanuy@gmail.com>
2022-03-06 19:28:45 +0100

    Format unlink.test.cpp

--- tests/mq/unlink.test.cpp
+++ tests/mq/unlink.test.cpp
@@ -6,7 +6,7 @@ using namespace mq_constants;
 
 TEST_CASE("unlinking non existing queue") {
   auto result = mq::unlink("/unlink");
-  CHECK_FALSE( result);
+  CHECK_FALSE(result);
   CHECK(result.error() == mq::UnlinkError::queue_missing);
 }
 TEST_CASE("creating and unlinking queue") {
@@ -16,12 +16,11 @@ TEST_CASE("creating and unlinking queue") {
   CHECK(result);
 }
 TEST_CASE("creating unsuccessfully and unlinking queue") {
-  auto name =
-      "/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  auto name   = "/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
   auto queue  = mq::open(name, read_only | create, 0666);
   auto result = mq::unlink(name);
-  CHECK_FALSE( result);
+  CHECK_FALSE(result);
   CHECK(result.error() == mq::UnlinkError::name_too_long);
 }
