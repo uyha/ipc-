7d0ab238a2f9f982336f639ab81b1c2a67ae0043
Uy Ha <hchanuy@gmail.com>
2022-03-10 22:03:42 +0100

    Turn test case to section in mq/unlink.test.cpp

--- tests/mq/unlink.test.cpp
+++ tests/mq/unlink.test.cpp
@@ -4,23 +4,25 @@
 using namespace lpipp;
 using namespace mq_constants;
 
-TEST_CASE("unlinking non existing queue") {
-  auto result = mq::unlink("/unlink");
-  CHECK_FALSE(result);
-  CHECK(result.error() == mq::UnlinkError::queue_missing);
-}
-TEST_CASE("creating and unlinking queue") {
-  auto name   = "/unlink";
-  auto queue  = mq::open(name, read_only | create, 0666);
-  auto result = mq::unlink(name);
-  CHECK(result);
-}
-TEST_CASE("creating unsuccessfully and unlinking queue") {
-  auto name   = "/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-  auto queue  = mq::open(name, read_only | create, 0666);
-  auto result = mq::unlink(name);
-  CHECK_FALSE(result);
-  CHECK(result.error() == mq::UnlinkError::name_too_long);
+TEST_CASE("unlinking message queue") {
+  SECTION("unlinking non-existing queue should fail") {
+    auto result = mq::unlink("/unlink");
+    CHECK_FALSE(result);
+    CHECK(result.error() == mq::UnlinkError::queue_missing);
+  }
+  SECTION("creating and unlinking queue") {
+    auto name   = "/unlink";
+    auto queue  = mq::open(name, read_only | create, 0666);
+    auto result = mq::unlink(name);
+    CHECK(result);
+  }
+  SECTION("creating unsuccessfully and unlinking queue") {
+    auto name   = "/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    auto queue  = mq::open(name, read_only | create, 0666);
+    auto result = mq::unlink(name);
+    CHECK_FALSE(result);
+    CHECK(result.error() == mq::UnlinkError::name_too_long);
+  }
 }
