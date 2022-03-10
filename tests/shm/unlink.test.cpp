8f74afa6f32053ab79135c0d8b8236665982ba97
Uy Ha <hchanuy@gmail.com>
2022-03-10 22:31:56 +0100

    Add test for `shm::unlink`

---
+++ tests/shm/unlink.test.cpp
@@ -0,0 +1,28 @@
+#include <catch2/catch.hpp>
+#include <lpipp/shm.hpp>
+
+using namespace lpipp;
+using namespace shm_constants;
+
+TEST_CASE("unlinking shared memory") {
+  SECTION("unlinking non-existing queue should fail") {
+    auto result = shm::unlink("/shm.unlink");
+    CHECK_FALSE(result);
+    CHECK(result.error() == shm::UnlinkError::shared_memory_missing);
+  }
+  SECTION("creating and unlinking queue") {
+    auto name          = "/shm.unlink.open";
+    auto shared_memory = shm::open(name, read_only | create, 0666);
+    auto result        = shm::unlink(name);
+    CHECK(result);
+  }
+  SECTION("creating unsuccessfully and unlinking queue") {
+    auto name   = "/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    auto queue  = shm::open(name, read_only | create, 0666);
+    auto result = shm::unlink(name);
+    CHECK_FALSE(result);
+    CHECK(result.error() == shm::UnlinkError::shared_memory_missing);
+  }
+}
