feccebd8f98eb146b03436f161998fa7c43ad629
Uy Ha <hchanuy@gmail.com>
2022-05-08 20:03:13 +0200

    chore: format file

--- include/lpipp/concepts.hpp
+++ include/lpipp/concepts.hpp
@@ -8,13 +8,13 @@ concept is_any_of = (std::is_same_v<T, Ts> || ...);
 // clang-format off
 template <typename T>
 concept socket_option = std::is_same_v<std::remove_cv_t<decltype(T::level)>, int> &&
-                       std::is_same_v<std::remove_cv_t<decltype(T::name)>, int> &&
-                       std::is_same_v<std::remove_cv_t<decltype(T::read)>, bool> &&
-                       std::is_same_v<std::remove_cv_t<decltype(T::write)>, bool> &&
-                       requires (T option){
-                        {option.value};
-                        {option.size};
-                       };
+                        std::is_same_v<std::remove_cv_t<decltype(T::name)>, int> &&
+                        std::is_same_v<std::remove_cv_t<decltype(T::read)>, bool> &&
+                        std::is_same_v<std::remove_cv_t<decltype(T::write)>, bool> &&
+                        requires (T option){
+                          {option.value};
+                          {option.size};
+                        };
 // clang-format on
 template <typename T>
 concept socket_read_option = socket_option<T> && T::read;
